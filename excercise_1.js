88aeafd2f1b2b9d22fe0c1f7505f9f939e3d1379
Trongle21 <Trongleele2@gmail.com>
2023-04-06 19:55:46 +0700

    second commit

--- excercise_1.js
+++ excercise_1.js
@@ -1,22 +1,22 @@
-/**Viết phương trình bậc 2 */
+// /**Viết phương trình bậc 2 */
 
-// ax^2 + bx + c = 0
+// // ax^2 + bx + c = 0
 
-function quadratic_equation(a, b, c) {
-    let delta = b * b - 4 * a * c;
-    let x1, x2;
-    if (delta < 0) {
-        return ('Phương trình vô nghiệm')
-    }
-    if (delta == 0) {
-        x1 = -b / 2 * a;
-        x2 = -b / 2 * a;
-        return ('Phương trình có 2 nghiệm x1 = x2 =  ' + x1)
-    }
-    if (delta > 0) {
-        x1 = (-b + Math.sqrt(delta)) / 2;
-        x2 = (-b - Math.sqrt(delta)) / 2
-        return ('Phương trình có 2 ngiệm: x1= ' + x1 + 'và x2 = ' + x2)
-    }
-}
-console.log(quadratic_equation(2, -7, 3))
+// function quadratic_equation(a, b, c) {
+//     let delta = b * b - 4 * a * c;
+//     let x1, x2;
+//     if (delta < 0) {
+//         return ('Phương trình vô nghiệm')
+//     }
+//     if (delta == 0) {
+//         x1 = -b / 2 * a;
+//         x2 = -b / 2 * a;
+//         return ('Phương trình có 2 nghiệm x1 = x2 =  ' + x1)
+//     }
+//     if (delta > 0) {
+//         x1 = (-b + Math.sqrt(delta)) / 2;
+//         x2 = (-b - Math.sqrt(delta)) / 2
+//         return ('Phương trình có 2 ngiệm: x1= ' + x1 + 'và x2 = ' + x2)
+//     }
+// }
+// console.log(quadratic_equation(0, -2, 4))
---
+++ excercise_2.js
@@ -0,0 +1,23 @@
+// indexOf()
+//split()
+//reverse()
+//join()
+
+// Viết function khi truyền vào 1 số tự nhiên. nếu nó được đọc theo hướng từ trái sang phải hoặc từ phải qua trái đều giữ được giá trị
+
+function is_palinddrome(x) {
+    if (typeof x !== 'number') {
+        console.log('Đây không phải số tự nhiên');
+    }
+    let num = x.toString();
+    let arrNum = num.split('');
+    let reverseNum = arrNum.reverse().join("");
+    let newNum = parseInt(reverseNum);
+    if (newNum === x) {
+        console.log('Số có thể đảo ngược')
+    } else {
+        console.log('số không thể đảo ngược')
+    }
+    return;
+}
+console.log(is_palinddrome(342))
